e5970712d90d3206114e614d46df67c99738181c
szare <szarekwiktor13@gmail.com>
2026-03-17 21:50:12 +0100

    ZL-3: Day information

---
+++ src/zl3/Main.java
@@ -0,0 +1,56 @@
+package zl3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe z zakresu 1-7 odpowiadająca dniu tygodnia");
+        int day = scanner.nextInt();
+        scanner.nextLine();
+
+        switch(day) {
+            case 1 -> {
+                System.out.println("Poniedziałek");
+            }
+            case 2 -> {
+                System.out.println("Wtorek");
+            }
+            case 3 -> {
+                System.out.println("Środa");
+            }
+            case 4 -> {
+                System.out.println("Czwartek");
+            }
+            case 5 -> {
+                System.out.println("Piątek");
+            }
+            case 6 -> {
+                System.out.println("Sobota");
+            }
+            case 7 -> {
+                System.out.println("Niedziela");
+            }
+            default -> System.out.println("Liczba spoza zakresu");
+        }
+
+        switch(day) {
+            case 1,2,3,4,5 -> {
+                System.out.println("Jest to dzień roboczy");
+                System.out.println("Autobus odjeżdza o 05:30");
+            }
+            case 6 -> {
+                System.out.println("Jest to weekend");
+                System.out.println("Autobus odjeżdza o 07:00");
+            }
+            case 7 -> {
+                System.out.println("Jest to weekend");
+                System.out.println("Autobus odjeżdza o 09:00");
+            }
+            default -> {
+                System.out.println("Brak informacji");
+            }
+        }
+
+    }
+}
