bde1ed37dd3fdece720f8357ef6fddffbd79b06a
szare <szarekwiktor13@gmail.com>
2026-03-19 19:59:58 +0100

    PD-4: implement player stats, leaderboard, prime numbers, and pyramid

---
+++ src/pd4/Main.java
@@ -0,0 +1,58 @@
+package pd4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static int getIndexOfMaxScore(Player[] players) {
+        int indexOfMaxScore = 0;
+        int largestScore = players[0].getLargestScore();
+        for (int i = 1; i < players.length; i++) {
+            if (players[i].getLargestScore() > largestScore) {
+                largestScore = players[i].getLargestScore();
+                indexOfMaxScore = i;
+            }
+        }
+        return indexOfMaxScore;
+    }
+
+    public static int[] getScoreFromUser() {
+        int[] scores = new int[3];
+        for (int j = 0; j < 3; j++) {
+            System.out.printf("Podaj %d wynik: ", j + 1);
+            scores[j] = scanner.nextInt();
+            scanner.nextLine();
+        }
+        return scores;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Podaj liczbę graczy");
+        int amountOfPlayers = scanner.nextInt();
+        scanner.nextLine();
+        Player[] players = new Player[amountOfPlayers];
+        for (int i = 0; i < amountOfPlayers; i++) {
+            System.out.printf("Podaj imię gracza numer %d: ", i + 1);
+            String name = scanner.nextLine();
+            players[i] = new Player(name, getScoreFromUser());
+
+        }
+        Arrays.sort(players);
+        for (int i = 0; i < players.length; i++) {
+            if (i == getIndexOfMaxScore(players)) {
+                System.out.printf("%d.%s*%n", i + 1, players[i]);
+            } else {
+                System.out.printf("%d.%s%n", i + 1, players[i]);
+            }
+        }
+
+        for (int i = 0; i < players.length; i++) {
+            System.out.printf("%d miejsce %s%n", i + 1, players[i].getName());
+        }
+        scanner.close();
+    }
+
+}
---
+++ src/pd4/Task2.java
@@ -0,0 +1,37 @@
+package pd4;
+
+public class Task2 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 50; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+
+
+        printPiramid();
+    }
+
+    private static boolean isPrime(int number) {
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        for (int j = 3; j < number; j += 2) {
+            if (number % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private static void printPiramid(){
+        for (int i = 0; i < 5; i++) {
+            if (i == 0) {
+                System.out.println("*");
+            } else if (i == 1) {
+                System.out.println(" ");
+            } else {
+                System.out.println("**");
+            }
+        }
+    }
+}
