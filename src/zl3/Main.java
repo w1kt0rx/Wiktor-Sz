644648ea07060c1235af89b9581c6f6870303b62
szare <szarekwiktor13@gmail.com>
2026-03-19 15:14:40 +0100

    ZL-3: Fixed brackets in switch expression and added throwing exception

--- src/zl3/Main.java
+++ src/zl3/Main.java
@@ -7,35 +7,28 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj liczbe z zakresu 1-7 odpowiadająca dniu tygodnia");
         int day = scanner.nextInt();
-        scanner.nextLine();
 
-        switch(day) {
-            case 1 -> {
-                System.out.println("Poniedziałek");
-            }
-            case 2 -> {
-                System.out.println("Wtorek");
-            }
-            case 3 -> {
-                System.out.println("Środa");
-            }
-            case 4 -> {
-                System.out.println("Czwartek");
-            }
-            case 5 -> {
-                System.out.println("Piątek");
-            }
-            case 6 -> {
-                System.out.println("Sobota");
-            }
-            case 7 -> {
-                System.out.println("Niedziela");
-            }
-            default -> System.out.println("Liczba spoza zakresu");
+
+        switch (day) {
+            case 1 -> System.out.println("Poniedziałek");
+
+            case 2 -> System.out.println("Wtorek");
+
+            case 3 -> System.out.println("Środa");
+
+            case 4 -> System.out.println("Czwartek");
+
+            case 5 -> System.out.println("Piątek");
+
+            case 6 -> System.out.println("Sobota");
+
+            case 7 -> System.out.println("Niedziela");
+
+            default -> throw new RuntimeException("Wartość spoza zakresu");
         }
 
-        switch(day) {
-            case 1,2,3,4,5 -> {
+        switch (day) {
+            case 1, 2, 3, 4, 5 -> {
                 System.out.println("Jest to dzień roboczy");
                 System.out.println("Autobus odjeżdza o 05:30");
             }
@@ -46,9 +39,6 @@ public class Main {
             case 7 -> {
                 System.out.println("Jest to weekend");
                 System.out.println("Autobus odjeżdza o 09:00");
-            }
-            default -> {
-                System.out.println("Brak informacji");
             }
         }
 
