d44741ba8ddafccb14e63ac50e49c166f2581d00
szare <szarekwiktor13@gmail.com>
2026-03-25 16:14:55 +0100

    PD-6: added MathLibrary class with a couple of methods

---
+++ src/pd6/MathLibrary.java
@@ -0,0 +1,150 @@
+package pd6;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class MathLibrary {
+    static Scanner scanner = new Scanner(System.in);
+
+    static long factorialIteration(final int n) {
+        if (n == 0) {
+            return 1;
+        }
+        long factorial = 1;
+        for (int i = 2; i <= n; i++) {
+            factorial *= i;
+
+        }
+        return factorial;
+    }
+
+
+    static long factorialRecursion(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorialRecursion(n - 1);
+    }
+
+    static boolean isPrime(int n) {
+        double sqrtOfNumber = Math.sqrt(n);
+        for (int i = 2; i <= sqrtOfNumber; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static int[] sieveOfEratosthenes(int limit) {
+        int[] tabOfNumbers = new int[limit - 2];
+        int amountOfDigitsToDelete = 0;
+        //filling array with numbers from 2 to limit
+        for (int i = 2; i < limit; i++) {
+            tabOfNumbers[i - 2] = i;
+        }
+        //finding numbers that multiplicity of prime numbers and setting them to 1
+        for (int i = 2; i < limit; i++) {
+            if (tabOfNumbers[i - 2] != 1 || i * i < limit) {
+                for (int j = i + 1; j < limit; j++) {
+                    if (tabOfNumbers[j - 2] % i == 0) {
+                        tabOfNumbers[j - 2] = 1;
+                        amountOfDigitsToDelete++;
+                    }
+                }
+            }
+        }
+        //creating new array and filling it with numbers from tabOfNumbers that are not 1
+        int[] sieveOfEratosthenes = new int[tabOfNumbers.length - amountOfDigitsToDelete];
+        int amountOfDigitsInSieve = 0;
+        for (int i = 0; i < tabOfNumbers.length; i++) {
+            if (tabOfNumbers[i] != 1) {
+                sieveOfEratosthenes[amountOfDigitsInSieve] = tabOfNumbers[i];
+                amountOfDigitsInSieve++;
+            }
+        }
+        return sieveOfEratosthenes;
+    }
+
+
+    static int gcd(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return gcd(b, a % b);
+    }
+
+    static void menu() {
+        boolean isFinished = false;
+        System.out.println("Witam w interaktywnym menu");
+        while (!isFinished) {
+            System.out.println("1.Iteracyjna silnia");
+            System.out.println("2.Rekurencyjna silnia");
+            System.out.println("3.Sprawdzenie czy liczba jest liczbą pierwszą");
+            System.out.println("4.Utworzenie sita Eratostenesa");
+            System.out.println("5.Obliczenie największego wspólnego dzielnika.");
+            System.out.println("6.Wyjście");
+            System.out.println("Prosze podać liczbę odpowiadającą funkcji którą chcesz wybrać ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1 -> factorialIterationMenu();
+                case 2 -> factorialRecursionMenu();
+                case 3 -> isPrimeMenu();
+                case 4 -> sieveOfEratosthenesMenu();
+                case 5 -> gcdMenu();
+                case 6 -> isFinished = true;
+                default -> System.out.println("Nie ma takiej opcji");
+            }
+            System.out.println("Czy chcesz wyjść z programu?");
+            System.out.println("1-tak, enter-nie");
+            if(scanner.nextLine().equals("1")){
+                isFinished = true;
+            }
+        }
+        System.out.println("Żegnam");
+
+    }
+
+    static void factorialIterationMenu() {
+        System.out.println("Prosze podac liczbę z której mamy obliczyć silnie");
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        System.out.printf("Wynik silni z liczby %d to: %d%n", number, factorialIteration(number));
+    }
+
+    static void factorialRecursionMenu() {
+        System.out.println("Prosze podac liczbę z której mamy obliczyć silnie");
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        System.out.printf("Wynik silni z liczby %d to: %d%n", number, factorialRecursion(number));
+    }
+
+    static void isPrimeMenu() {
+        System.out.println("Prosze podac liczbę którą mam sprawdzić");
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        System.out.printf("Liczba %d %s%n", number, isPrime(number) ? "jest liczbą pierwszą" : "nie jest liczbą pierwszą");
+    }
+
+    static void sieveOfEratosthenesMenu() {
+        System.out.println("Prosze podac liczbę która będzie granicą sita Eratostenesa");
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        int[] sieve = sieveOfEratosthenes(number);
+        System.out.println("Sito Eratostenesa jest przedstawione poniżej");
+        for (int i = 0; i < sieve.length; i++) {
+            System.out.print(sieve[i] + ",");
+        }
+        System.out.println();
+    }
+
+    static void gcdMenu() {
+        System.out.println("Prosze podac dwie liczby do obliczenia NWD");
+        int number = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        scanner.nextLine();
+        System.out.printf("Największy wspólny dzielnik liczb %d, %d to: %d%n",number, number2, gcd(number, number2));
+    }
+
+}
